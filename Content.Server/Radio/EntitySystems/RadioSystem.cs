d1b4af10cefb9981b50238b92d07d620acf71e15
MetalSage <metalsage.official@gmail.com>
2025-01-15 13:26:46 +0400

    add sound for radio

--- Content.Server/Radio/EntitySystems/RadioSystem.cs
+++ Content.Server/Radio/EntitySystems/RadioSystem.cs
@@ -8,6 +8,9 @@ using Content.Shared.Database;
 using Content.Shared.Radio;
 using Content.Shared.Radio.Components;
 using Content.Shared.Speech;
+using Robust.Shared.Audio;
+using Robust.Shared.Audio.Systems;
+using Robust.Shared.Audio.Systems;
 using Robust.Shared.Map;
 using Robust.Shared.Network;
 using Robust.Shared.Player;
@@ -29,6 +32,7 @@ public sealed class RadioSystem : EntitySystem
     [Dependency] private readonly IPrototypeManager _prototype = default!;
     [Dependency] private readonly IRobustRandom _random = default!;
     [Dependency] private readonly ChatSystem _chat = default!;
+    [Dependency] private readonly SharedAudioSystem _audio = default!;
 
     // set used to prevent radio feedback loops.
     private readonly HashSet<string> _messages = new();
@@ -151,6 +155,9 @@ public sealed class RadioSystem : EntitySystem
             RaiseLocalEvent(receiver, ref attemptEv);
             if (attemptEv.Cancelled)
                 continue;
+
+            if (HasComp<MarineComponent>(messageSource))
+                _audio.PlayPvs("/Audio/_Stories/Effects/radiostatic.ogg", messageSource, AudioParams.Default.WithVariation(0.1f).WithMaxDistance(1.0f).WithVolume(0.5f));
 
             // send the message
             RaiseLocalEvent(receiver, ref ev);
